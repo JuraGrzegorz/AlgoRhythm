1ca2f6eb4b2a742757619e4a16f736e5d7f6e943
MaciejKrakowiak <240719@edu.p.lodz.pl>
2024-06-13 22:02:30 +0200

    no need to login on start if u were loggedin before

--- app/src/main/java/com/example/algorythm/MainNavigation.kt
+++ app/src/main/java/com/example/algorythm/MainNavigation.kt
@@ -1,7 +1,10 @@
 package com.example.algorythm
 
+import android.app.Activity
+import android.content.Context
 import android.graphics.Bitmap
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -9,11 +12,15 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun SignInNavigation(){
     val navigationController = rememberNavController()
-
+    var startDest  = Screens.Home.screen
+    val activity = LocalContext.current as Activity
+    var jwt = ""
+    val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
+    jwt = sharedPref.getString("JWT","") ?:""
+    if (jwt.isEmpty()) startDest = Screens.SignInScreen.screen
     NavHost(
-
         navController = navigationController,
-        startDestination = Screens.SignInScreen.screen,
+        startDestination = startDest,
     ) {
         composable(Screens.SignInScreen.screen) { SignInScreen(navigationController) }
         composable(Screens.SignUpScreen.screen) { SignUpScreen(navigationController) }
